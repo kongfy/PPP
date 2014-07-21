449f67f4cf4f5784eae1324ddd6d6e45e3ddf336
Fanyu Kong <njukongfy@gmail.com>
2014-07-21 16:24:38 +0800

    update configs

--- batch.py
+++ batch.py
@@ -3,7 +3,6 @@
 import os
 import os.path
 import shutil
-import time
 
 def configs():
     for root, dirs, files in os.walk('configs'):
@@ -12,22 +11,23 @@ def configs():
 
 def main():
     config_filename = os.path.join(os.getcwd(), 'config.py')
+    config_pyc = config_filename + 'c'
     result_dir = os.path.join(os.getcwd(), 'results')
     for config in configs():
+        if os.path.exists(config_pyc):
+            os.remove(config_pyc)
+
         shutil.copy(config, config_filename)
-        time.sleep(3)
-        
+
         if not os.path.exists(result_dir):
             os.mkdir(result_dir)
-        
+
         output = os.path.basename(config)[:os.path.basename(config).rfind('.')] + '.out'
         cmd = 'python main.py > ' + os.path.join(result_dir, output)
         
         print cmd
         os.system(cmd)
         
-        
-
 if __name__ == '__main__':
     main()
         
